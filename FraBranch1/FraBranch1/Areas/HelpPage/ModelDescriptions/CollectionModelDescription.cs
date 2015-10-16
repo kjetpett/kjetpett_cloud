8b343c10b24ade495d229220894582e81018afec
kjetpett <kjetil.pettersson@gmail.com>
2015-10-16 12:09:41 -0500

    FraBranch1 solution opprettet

---
+++ FraBranch1/FraBranch1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FraBranch1.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
